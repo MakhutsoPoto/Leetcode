2d2d1530a7f43eee2183e71744f2fef6a3a2c00e
Makhutso Poto <makhutsopotoofficial@gmail.com>
2025-03-15 21:06:06 +0200

    Time: 14 ms (46.85%) | Memory: 18.7 MB (11.82%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(t) == sorted(s):
+            return True
+        return False
+          
+            
+
+        
